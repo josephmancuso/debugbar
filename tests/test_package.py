179e95e6ac93bd9be2d3646a77913aba0922f9c8
Joseph Mancuso <idmann509@gmail.com>
2021-12-27 19:47:49 -0500

    Initial commit

---
+++ tests/test_package.py
@@ -0,0 +1,10 @@
+
+# class TestPackage(TestCase):
+
+#     def setUp(self):
+#         super().setUp()
+    
+#     def test_can_get_home_route(self):
+#         self.assertTrue(
+#             self.get('/home').contains('Hello World')
+#         )
